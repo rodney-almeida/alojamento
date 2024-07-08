03ccbf02766e5fea8d633c4647ae8d15727db424
Rodney Almeida <rodney.almeida@tekenable.com>
2024-07-08 09:57:59 +0100

    Update image file extensions to use .jpg instead of .png

--- js/encode-contacts.js
+++ js/encode-contacts.js
@@ -1,15 +1,1 @@
-var encEmail = "Y2FzYWFydm9yYUBnbWFpbC5jb20=";
-var encPhone = "KzM1MSA5MTQgODM5IDYzNg==";
-var encPhoneNoSpace = "KzM1MTkxNDgzOTYzNg==";
-
-const emailLink = document.getElementById("contact-email");
-emailLink.setAttribute("href", "mailto:".concat(atob(encEmail)));
-
-const emailElement = document.getElementById("contact-email-text");
-emailElement.innerHTML = atob(encEmail);
-
-const phoneLink = document.getElementById("contact-phone");
-phoneLink.setAttribute("href", "tel://".concat(atob(encPhoneNoSpace)));
-
-const phoneElement = document.getElementById("contact-phone-text");
-phoneElement.innerHTML = atob(encPhone);
+var encEmail="Y2FzYWFydm9yYUBnbWFpbC5jb20=";var encPhone="KzM1MSA5MTQgODM5IDYzNg==";var encPhoneNoSpace="KzM1MTkxNDgzOTYzNg==";const emailLink=document.getElementById("contact-email");emailLink.setAttribute("href","mailto:".concat(atob(encEmail)));const emailElement=document.getElementById("contact-email-text");emailElement.innerHTML=atob(encEmail);const phoneLink=document.getElementById("contact-phone");phoneLink.setAttribute("href","tel://".concat(atob(encPhoneNoSpace)));const phoneElement=document.getElementById("contact-phone-text");phoneElement.innerHTML=atob(encPhone)
--- js/google-tag.js
+++ js/google-tag.js
@@ -1,5 +1,2 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-6FMT1ZEJBX');
+window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}
+gtag('js',new Date());gtag('config','G-6FMT1ZEJBX')
