99751e2781d3f4cb766a3419bb63298c8966efab
Rodney Almeida <rodney.almeida@tekenable.com>
2024-07-11 20:48:15 +0100

    Update meta description in HTML files for multiple languages

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
 function getYear(){var currentDate=new Date();var currentYear=currentDate.getFullYear();document.querySelector("#displayYear").innerHTML=currentYear}
 getYear();function openNav(){document.getElementById("myNav").classList.toggle("menu_width");document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")}
-$(document).on("click",'[data-toggle="lightbox"]',function(event){event.preventDefault();$(this).ekkoLightbox()});function getDate(){var today=new Date().toISOString().split("T")[0];document.getElementById("date").value=today}
+$(document).on("click",'[data-toggle="lightbox"]',function(event){event.preventDefault();$(this).ekkoLightbox()});function getDate(){var dateElement=document.getElementById("date");if(dateElement){var today=new Date().toISOString().split("T")[0];dateElement.value=today}}
 getDate()
